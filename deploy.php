3c037c178c15818db7a1796e82680cd77337f903
Cyron Mark <cyronmark228@gmail.com>
2025-05-02 00:15:00 +0800

    add deploy.php for  auto deploy the file

---
+++ deploy.php
@@ -0,0 +1,18 @@
+<?php
+$secret = 'your-secret-key';
+$headers = getallheaders();
+
+if ($headers['X-Hub-Signature-256'] ?? false) {
+    $payload = file_get_contents('php://input');
+    $sig = 'sha256=' . hash_hmac('sha256', $payload, $secret);
+    if (!hash_equals($sig, $headers['X-Hub-Signature-256'])) {
+        http_response_code(403);
+        exit('Invalid signature');
+    }
+}
+
+$output = [];
+exec('cd /home/zydebnwqs4su/git-repo-path && git pull 2>&1', $output);
+file_put_contents('/home/zydebnwqs4su/deploy.log', implode("\n", $output), FILE_APPEND);
+echo "Deployed.";
+?>
